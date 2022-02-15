46dd23d541a787eb9e796cf2617267808a532c96
zaini <alimzaini@outlook.com>
2022-02-15 23:45:48 +0000

    Add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Heading } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Container>
+      <Heading>righermove</Heading>
+    </Container>
+  );
+};
+
+export default App;
