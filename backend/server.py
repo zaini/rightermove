35a250c6891d49f3d9127ec00b251f66e33bab48
zaini <alimzaini@outlook.com>
2022-02-16 18:19:29 +0000

    Add caching to backend

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,7 @@ import googlemaps
 from rightmove_webscraper import RightmoveData
 from flask import Flask, request
 from flask_cors import CORS
+from utils.lfu_cache import lfu_cache
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +14,9 @@ GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
 
 
+@lfu_cache(maxsize=512)
 def get_transit_time(start, end, arrival_time=1644915600):
+    print("get_transit_time", start, end)
     directions_result = gmaps.directions(start,
                                          end,
                                          mode="transit",
@@ -24,13 +27,9 @@ def get_transit_time(start, end, arrival_time=1644915600):
         return -1
 
 
-@app.route('/properties', methods=['GET'])
-def get_properties():
-    args = request.args
-    url = args['url']
-    end_address = args['address']
-
-    url = urllib.parse.unquote(url)
+@lfu_cache(maxsize=128)
+def get_rightmove_properties(url, end_address):
+    print("get_rightmove_properties")
     rm = RightmoveData(url)
     results = rm.get_results
 
@@ -38,6 +37,18 @@ def get_properties():
         address, end_address) // 60 for address in results['address']]
 
     return {'properties': results.to_json(orient='records')}
+
+
+@app.route('/properties', methods=['GET'])
+def get_properties():
+    print("get_properties")
+    args = request.args
+    url = args['url']
+    end_address = args['address']
+
+    url = urllib.parse.unquote(url)
+
+    return get_rightmove_properties(url, end_address)
 
 
 @app.route('/properties/summary', methods=['GET'])
